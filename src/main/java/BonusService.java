09f5644fcca8e52fe47d1541fd0e893d707083e6
Zanahoria <frolmt@gmail.com>
2025-02-22 15:04:14 +0300

    Fix #1

--- src/main/java/BonusService.java
+++ src/main/java/BonusService.java
@@ -1,8 +1,8 @@
 public class BonusService {
     // Раасчет бонуса при пополнеиии зарегистрированого и незарегистрированого пользователя
     public long calculate(long amount, boolean registered) {
-        int Percent = registered ? 3 : 1;  // тернарное выражение
-        long bonus = amount * Percent / 100;
+        int percent = registered ? 3 : 1;  // тернарное выражение
+        long bonus = amount * percent / 100;
         long limit = 500; // макс лимит бонусов
         if (bonus > limit) {
             bonus = limit;
@@ -10,3 +10,4 @@ public class BonusService {
         return bonus;
     }
 }
+
