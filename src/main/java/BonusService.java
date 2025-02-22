96e907ce6e153c7f6d7be40698cb9c7a19b05493
Zanahoria <frolmt@gmail.com>
2025-02-22 13:42:31 +0300

    pow.changed

--- src/main/java/BonusService.java
+++ src/main/java/BonusService.java
@@ -1,8 +1,8 @@
 public class BonusService {
     // Раасчет бонуса при пополнеиии зарегистрированого и незарегистрированого пользователя
     public long calculate(long amount, boolean registered) {
-        int percent = registered ? 3 : 1;  // тернарное выражение
-        long bonus = amount * percent / 100;
+        int Percent = registered ? 3 : 1;  // тернарное выражение
+        long bonus = amount * Percent / 100;
         long limit = 500; // макс лимит бонусов
         if (bonus > limit) {
             bonus = limit;
